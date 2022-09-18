725e73a4811e622ff092e234baa3563d05c3d642
KonstantinSF <kosmos008@gmail.com>
2022-09-18 16:48:12 +0300

    1

--- Fibonachi/Source.cpp
+++ Fibonachi/Source.cpp
@@ -4,5 +4,16 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, ""); 
+	int limit_num_fibbonacci; 
+	cout << "Введите предел, до которого хотите увидеть ряд Фибоначчи:"; cin >> limit_num_fibbonacci; 
+	int* Fibonacci = new int[limit_num_fibbonacci];
+	for (int i = 0; i < limit_num_fibbonacci; i++)
+	{
+		if (i==0) Fibonacci[0] = 0; 
+		else if (i==1) Fibonacci[1] = 1; 
+		else Fibonacci[i] = Fibonacci[i - 2] + Fibonacci[i - 1];
+	}
+	delete[] Fibonacci; 
+
 
 }
